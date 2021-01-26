9bf1301a9ddcb0b0a7a5a81b2dd86a576af95bc4
absens-x <absens.x@gmail.com>
2021-01-26 17:04:00 +0300

    add: calculate module & big.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,148 @@
-import React from 'react';
+import * as React from 'react';
 
-import './App.css';
+import Display from './components/Display/Display';
+import ButtonGroup from './components/UI/ButtonGroup/ButtonGroup';
+import Button, { IButton } from './components/UI/Button/Button';
+import calculate from './calculate';
 
-function App() {
+import './App.scss';
+import './sass/main.scss';
+
+type StateValueType = string | null;
+
+export interface ICalcState {
+    total?: StateValueType;
+    next?: StateValueType;
+    operation?: StateValueType;
+}
+
+const App: React.FC = () => {
+    const [calcState, setCalcState] = React.useState<ICalcState>({
+        total: null,
+        next: null,
+        operation: null,
+    });
+
+    const buttonsList: IButton[] = [
+        {
+            name: 'C',
+            title: 'C',
+            primary: true,
+        },
+        {
+            name: 'CE',
+            title: 'CE',
+            primary: true,
+        },
+        {
+            name: 'percent',
+            title: '%',
+        },
+        {
+            name: 'del',
+            title: 'x',
+        },
+        {
+            name: '7',
+            title: '7',
+        },
+        {
+            name: '8',
+            title: '8',
+        },
+        {
+            name: '9',
+            title: '9',
+        },
+        {
+            name: 'div',
+            title: '/',
+        },
+        {
+            name: '4',
+            title: '4',
+        },
+        {
+            name: '5',
+            title: '5',
+        },
+        {
+            name: '6',
+            title: '6',
+        },
+        {
+            name: 'times',
+            title: '*',
+        },
+        {
+            name: '1',
+            title: '1',
+        },
+        {
+            name: '2',
+            title: '2',
+        },
+        {
+            name: '3',
+            title: '3',
+        },
+        {
+            name: 'minus',
+            title: '-',
+        },
+        {
+            name: 'plusminus',
+            title: '+/-',
+        },
+        {
+            name: '0',
+            title: '0',
+        },
+        {
+            name: '.',
+            title: '.',
+        },
+        {
+            name: 'plus',
+            title: '+',
+        },
+        {
+            name: 'pow',
+            title: 'pow',
+        },
+        {
+            name: 'sqrt',
+            title: 'sqrt',
+        },
+        {
+            name: 'equal',
+            title: '=',
+            wide: true,
+        },
+    ];
+
+    function handleClick(buttonName: string): void {
+        setCalcState((prevState) => {
+            const state: ICalcState = calculate(calcState, buttonName);
+            return {
+                ...prevState,
+                ...state,
+            };
+        });
+    }
+    console.log(calcState);
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+        <div className="App container">
+            <div className="wrapper">
+                <Display output={calcState.next || calcState.total || '0'} />
+                <ButtonGroup>
+                    {buttonsList.map((item, i) => {
+                        return <Button key={Math.random() * (i + 1)} {...item} clickHandler={handleClick} />;
+                    })}
+                </ButtonGroup>
+            </div>
         </div>
     );
-}
+};
 
 export default App;
---
+++ src/calculate.ts
@@ -0,0 +1,175 @@
+import Big, { BigSource } from 'big.js';
+import { ICalcState } from './App';
+
+/**
+ * Operate and return result
+ */
+
+Big.DP = 4;
+function operate(numOne: BigSource, numTwo: BigSource, operation: string) {
+    const one: BigSource = Big(numOne || '0');
+    const two: BigSource = Big(numTwo || (operation === '+' || operation === 'x' ? '1' : '0'));
+
+    if (operation === 'plus') {
+        return one.plus(two).toString();
+    }
+
+    if (operation === 'minus') {
+        return one.minus(two).toString();
+    }
+
+    if (operation === 'times') {
+        return one.times(two).toString();
+    }
+
+    if (operation === 'pow') {
+        return one.pow(+two).toString();
+    }
+
+    if (operation === 'sqrt') {
+        return two.sqrt().toString();
+    }
+
+    if (operation === 'div') {
+        if (two.toString() === '0') {
+            alert('Divide by 0 error');
+            return '0';
+        } else {
+            return one.div(two).toString();
+        }
+    }
+    throw Error(`Unknown operation '${operation}'`);
+}
+
+/**
+ * Calculate and return state
+ */
+
+export default function calculate(state: ICalcState, buttonName: string): ICalcState {
+    if (buttonName === 'C') {
+        return {
+            total: null,
+            next: null,
+            operation: null,
+        };
+    }
+
+    if (buttonName === 'CE') {
+        if (state.next) {
+            return {
+                next: '0',
+            };
+        }
+        return {};
+    }
+
+    if (/[0-9]+/.test(buttonName)) {
+        if (buttonName === '0' && state.next === '0') {
+            return {};
+        }
+
+        if (state.operation) {
+            if (state.next) {
+                return { next: state.next + buttonName };
+            }
+
+            return { next: buttonName };
+        }
+
+        if (state.next) {
+            const next: string = state.next === '0' ? buttonName : state.next + buttonName;
+
+            return {
+                next,
+                total: null,
+            };
+        }
+
+        return {
+            next: buttonName,
+            total: null,
+        };
+    }
+
+    if (buttonName === 'sqrt') {
+        if (state.next) {
+            return {
+                total: operate(0, state.next, 'sqrt'),
+                next: null,
+                operation: null,
+            };
+        }
+
+        return {};
+    }
+
+    if (buttonName === 'percent') {
+        if (state.operation && state.next) {
+            if (state.total) {
+                const result: BigSource = operate(state.total, state.next, state.operation);
+                return {
+                    total: Big(result).div(Big('100')).toString(),
+                    next: null,
+                    operation: null,
+                };
+            }
+        }
+
+        if (state.next) {
+            return {
+                next: Big(state.next).div(Big('100')).toString(),
+            };
+        }
+        return {};
+    }
+
+    if (buttonName === '.') {
+        if (state.next) {
+            if (state.next.includes('.')) {
+                return {};
+            }
+            return { next: state.next + '.' };
+        }
+        return { next: '0.' };
+    }
+
+    if (buttonName === 'equal') {
+        if (state.next && state.operation && state.total) {
+            return {
+                total: operate(state.total, state.next, state.operation),
+                next: null,
+                operation: null,
+            };
+        } else {
+            return {};
+        }
+    }
+
+    if (buttonName === 'plusminus') {
+        if (state.next) {
+            return { next: (-1 * parseFloat(state.next)).toString() };
+        }
+        if (state.total) {
+            return { total: (-1 * parseFloat(state.total)).toString() };
+        }
+        return {};
+    }
+
+    if (state.operation && state.total && state.next) {
+        return {
+            total: operate(state.total, state.next, state.operation),
+            next: null,
+            operation: buttonName,
+        };
+    }
+
+    if (!state.next) {
+        return { operation: buttonName };
+    }
+
+    return {
+        total: state.next,
+        next: null,
+        operation: buttonName,
+    };
+}
